ce6bd399d01ce4b372b574df55f8008496aa5f66
Documenter.jl <documenter@juliadocs.github.io>
2022-11-17 14:50:43 +0000

    build based on 055fb60

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = LibGEOSMakie","category":"page"},{"location":"#LibGEOSMakie","page":"Home","title":"LibGEOSMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for LibGEOSMakie.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [LibGEOSMakie]","category":"page"}]
+}
